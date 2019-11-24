732d16df6d7b40fb1884e1b3940febecfdbf9d90
reydon227 <dhruvarora@delhinerds.com>
2019-11-24 23:55:01 +0530

    constant sending to usame user fixed

--- telebot.py
+++ telebot.py
@@ -3,16 +3,18 @@ import json
 
 base_url = "https://api.telegram.org/bot808378125:AAEJxb5qJdSugysaGzxxHRsEQLDho-lcbcs/"
 users = {}
+already_replied = []
 approved_emails = ["infosys.com", "tcs.com", "mauna.ai"]
 
 def main():
+    print (already_replied, users)
     try:
         updates = requests.get("https://api.telegram.org/bot808378125:AAEJxb5qJdSugysaGzxxHRsEQLDho-lcbcs/getUpdates")
     except:
         print ("could not connect to telegram")
     else:
         updates = updates.json()
-        # print (updates)
+        print (updates)
         for each in updates["result"]:
             uid = each["message"]["from"]["id"]
             if uid in users.keys():
@@ -33,25 +35,31 @@ def replyToUser(data, uid):
         name = name + " " + last_name
     url = base_url + "sendMessage" 
 
-    # Checking user steps
-    if step == 0:
-        message = "Hi %s, let's get you into the anonymous social network, let's start by knowing your company email address %s" % (name, step)
-    if step == 1:
-        message = "Cool. I have sent you a secret password to your email address, please check and let me know the secret password for me to allow you in. %s" % step
-    if step == 2:  
-        message = "We have got your email address, and we will be adding you your company's anonymous channel. You will receive a confirmation shortly %s" % step
 
-    try:
-        data = {
-            "chat_id": data["message"]["chat"]["id"],
-            "text": message 
-        }
-        send_message = requests.post(url, data)
-    except:
-        print ("Could not send message")
+    # Check if the update is already replied to, if it is, don't send a message
+    if data["update_id"] in already_replied:
+        pass    
     else:
-        users[uid] += 1
-        print ("replies sent")
+    # Checking user steps
+        if step == 0:
+            message = "Hi %s, let's get you into the anonymous social network, let's start by knowing your company email address %s" % (name, step)
+        if step == 1:
+            message = "Cool. I have sent you a secret password to your email address, please check and let me know the secret password for me to allow you in. %s" % step
+        if step == 2:  
+            message = "We have got your email address, and we will be adding you your company's anonymous channel. You will receive a confirmation shortly %s" % step
+
+        try:
+            already_replied.push(data["update_id"])
+            data = {
+                "chat_id": data["message"]["chat"]["id"],
+                "text": message 
+            }
+            send_message = requests.post(url, data)
+        except:
+            print ("Could not send message")
+        else:
+            users[uid] += 1
+            print ("replies sent")
 
 
 main()        
